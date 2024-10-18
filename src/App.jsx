3d33211e751695f0f46414f7ff7a6e63714c176f
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2024-10-18 13:41:33 +0100

    changed common layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import {PaymentsDialog} from "./PaymentsDialog.jsx";
-import drake from "/img/drake.png";
+import logoMenu from "/img/logoMenu.png";
 import bg from "/img/bg.png";
-import line1 from "/img/line-1.png";
-import line2 from "/img/line-2.png";
-import line3 from "/img/line-3.png";
-import line4 from "/img/line-4.png";
 import {useAtom} from "jotai";
 import {calcMode, dialog} from "./main.jsx";
 import {Menu} from "./Menu.jsx";
@@ -17,42 +13,29 @@ function App() {
     const year = new Date().getFullYear()
     return <>
         <section className="tail">
-            <div className="line line-1" aria-hidden="true">
-                <img src={line1} alt="" loading="lazy"/>
-            </div>
-            <div className="line line-2" aria-hidden="true">
-                <img src={line2} alt="" loading="lazy"/>
-            </div>
-            <div className="line line-3" aria-hidden="true">
-                <img src={line3} alt="" loading="lazy"/>
-            </div>
-            <div className="line line-4" aria-hidden="true">
-                <img src={line4} alt="" loading="lazy"/>
-            </div>
             <div className={`tail__fakeOverlay ${displayed ? 'visible' : ''}`} aria-hidden="true"
                  style={{minHeight: overlayHeight}}/>
+
             <div className="container">
                 <div className="tail__bg" aria-hidden="true">
                     <img src={bg} alt="Creamier" className="tail__bg-image" loading="lazy"/>
                 </div>
+
                 <section className="tail__inner">
                     <header className="header">
-                        <div className="header__drake" aria-hidden="true">
-                            <img src={drake} alt="Creamier" className="header__drake-image"
+                        <div className="header__inner">
+                            <img src={logoMenu}
+                                 alt="Creamier"
+                                 className="header__logo"
                                  loading="lazy"/>
                         </div>
-                        <div className="header__inner">
-                            <h1 className="header__title">
-                                Menu
-                            </h1>
-                            <p className="header__subtitle">
-                                Pastry
-                            </p>
-                        </div>
                     </header>
+
                     <Menu/>
+
                     <section className="contacts">
                         <PaymentsDialog/>
+
                         <a href="https://instagram.com/creamier.porto/"
                            className="contacts__link contacts__link--instagram"
                            target="_blank" rel="noreferrer">
@@ -70,7 +53,7 @@ function App() {
         </section>
 
         {
-            showCalc ? <FloatTotal /> : null
+            showCalc ? <FloatTotal/> : null
         }
     </>
 }
